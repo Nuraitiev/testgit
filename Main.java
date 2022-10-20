5afc2ac74e448a841d8405075259fccc303b8735
nur-magomed <nur-magomed@mail.ru>
2022-10-20 22:55:02 +0300

    bug fix

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Salam from Git!");
+        System.out.println("Какое-то исправление на проде!");
     }
 }
