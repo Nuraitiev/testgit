79b2e1d363e403a8b87128cf7cd6f96ff832a69e
nur-magomed <nur-magomed@mail.ru>
2022-10-20 23:27:13 +0300

    added fix

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
         System.out.println("Какое-то исправление на проде!");
         System.out.println("На этом уроке мы рассмотрим что такое Git!");
         System.out.println("we finished, we can merge this to master!");
+        System.out.println("small fix!");
     }
 }
