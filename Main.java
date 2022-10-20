648d9a4a14f497028be001ccbfbcfad74021ee59
nur-magomed <nur-magomed@mail.ru>
2022-10-20 22:51:23 +0300

    rus text

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Salam from Git!");
+        System.out.println("На этом уроке мы рассмотрим что такое Git!");
     }
 }
